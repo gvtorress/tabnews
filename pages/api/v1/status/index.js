24a3087c5173e87cccd733ff8bfa537b48a2ec4d
Gabriel Torres <gvtorress@gmail.com>
2026-03-25 20:24:45 -0300

    feat: add properties in status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,32 @@
 import database from 'infra/database.js'
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1 as sum;');
-  console.log(result.rows);
-  response.status(200).json({ chave: 'alunos do curso.dev' })
+  const updatedAt = new Date().toISOString();
+  
+  const dbVersionResult = await database.query('SHOW server_version;');
+  const dbVersion = dbVersionResult.rows[0].server_version;
+
+  const dbMaxConnectionsResult = await database.query('SHOW max_connections;');
+  const dbMaxConnections = dbMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const dbOpenConnectionsResult = await database.query({
+    text: 'SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname=$1;',
+    values: [databaseName],
+  }
+  );
+  const dbOpenConnections = dbOpenConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersion,
+        max_connections: parseInt(dbMaxConnections),
+        open_connections: dbOpenConnections,
+      },
+    },
+  });
 }
 
 export default status;
