0f6e79652f225c5dd94c005f1f89a4e12f4719f5
Gabriel Torres <gvtorress@gmail.com>
2026-05-18 13:51:23 -0300

    feat: hash 'password' in 'user.create'

--- tests/integration/api/v1/users/[username]/get.test.js
+++ tests/integration/api/v1/users/[username]/get.test.js
@@ -34,7 +34,7 @@ describe("POST /api/v1/users/[username]", () => {
         id: response2Body.id,
         username: "MesmoCase",
         email: "mesmo.case@teste.com",
-        password: "senha123",
+        password: response2Body.password,
         created_at: response2Body.created_at,
         updated_at: response2Body.updated_at,
       });
@@ -68,7 +68,7 @@ describe("POST /api/v1/users/[username]", () => {
         id: response2Body.id,
         username: "CaseDiferente",
         email: "case.diferente@teste.com",
-        password: "senha123",
+        password: response2Body.password,
         created_at: response2Body.created_at,
         updated_at: response2Body.updated_at,
       });
